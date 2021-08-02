2f6f42b9853d8d73aee92d251a61cb5e705bea55
ryugit <ryudora925@icloud.com>
2021-08-03 02:32:04 +0900

    マイページのユーザー情報表示機能と一緒にプッシュし忘れていました。

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Utilities;
 class UserController extends Controller
 {
     public function store(Request $request)
@@ -20,6 +23,15 @@ class UserController extends Controller
         $user->save();
 
         return redirect('complete');
+    }
+
+    public function index()
+    {
+        $user_id = Auth::id();
+        $user_info = Auth::user();
+        $person_info = Profile::select('*')->find($user_id);
+        //dd($user_id,$user_info->part,$person_info);
+        return view('profile',['user_info' => $user_info,'person_info' => $person_info]);
     }
 
     //public function savePerson(Request $request) {
